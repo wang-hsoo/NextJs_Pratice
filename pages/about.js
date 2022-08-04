031f268dd82f84493fe2d0099ef04dbc83e76bad
wang-hsoo <dhkd4873@naver.com>
2022-08-04 13:29:14 +0900

    route

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar";
+
+export default function about(){
+  
+
+    return (
+        <div>
+            <NavBar />
+            <h1>about</h1>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import NavBar from "../components/NavBar";
+
+export default function Home(){
+    const [counter, setCounter] = useState(0);
+
+    return (
+        <div>
+            <NavBar />
+            <h1>Hello {counter}</h1>
+        </div>
+    )
+}
